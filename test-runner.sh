67e0d4b6bdd953d9037dfd6fa496e2a7749c438a
nomadbyte <nomadbyte@devbox>
2026-02-26 12:11:09 -0600

    Test lib

---
+++ test-runner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#nm -gU lib/osx/libTestJson.dylib | c++filt
---
+++ testlib.cpp
@@ -0,0 +1,22 @@
+//#include <testlib.h>
+
+#include <cstdlib>
+#include <cstdio>
+
+
+int runtests()
+{
+   int ret = 0;
+   return ret;
+}
+
+
+
+int main(int argc, char *argv[], char *envp[])
+{
+   int ret = 0;
+   ret = runtests();
+
+   return ret;
+}
+
